96ba0c3081e3a7df635c319ea44b008a56a64790
Christophe Hurpeau <christophe@hurpeau.com>
2012-12-09 12:02:24 +0100

    remove HHtml::jsCompat()

--- viewsLayouts/base.php
+++ viewsLayouts/base.php
@@ -3,7 +3,6 @@
 	<head>
 		<? HHtml::metaCharset() ?>
 		<title>{$layout_title}</title>
-		<? HHtml::jsCompat() ?>
 		<?php HHtml::cssLink(); HHtml::jsLink(); HHtml::jsI18n() ?>
 		<? HHtml::favicon() ?>
 	</head>
